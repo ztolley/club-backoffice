e42ec215216fa3e430279b93f61a409b4187ed99
Zac Tolley <zac@thetolleys.com>
2025-03-31 20:54:01 +0100

    Migrated User to use uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
+use App\Traits\HasUuid;
+
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
     use HasRoles;
+    use HasUuid;
 
     /**
      * The attributes that are mass assignable.
